676982c37a628b1aecb51bc8c3cbf1b7748a4a63
Gerardo Leyes Torres <gerardo.leyes@et.bo>
2021-07-23 10:45:33 -0400

    Proyecto inicial

---
+++ src/main/java/com/ce/seg/SegApplication.java
@@ -0,0 +1,13 @@
+package com.ce.seg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SegApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ce/seg/SegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ce.seg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
